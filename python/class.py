0bd15d745daf98173ebe7169bb3e7d22d34c07dc
zeitspare <96058214+zeitspare@users.noreply.github.com>
2022-11-25 14:30:17 +0100

    Update class.py

--- python/class.py
+++ python/class.py
@@ -1,8 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 25 13:30:02 2022
+
+@author: allservices
+"""
+
 class Rectangle:
+   # Defining attributes required for the class
    def __init__(self, length, breadth, unit_cost=0):
        self.length = length
        self.breadth = breadth
        self.unit_cost = unit_cost
+   # Functionality for the class
    def get_area(self):
        return self.length * self.breadth
    def calculate_cost(self):
@@ -10,14 +20,22 @@ class Rectangle:
        return area * self.unit_cost
    
 # breadth = 120 units, length = 160 units, 1 sq unit cost = Rs 2000
+
+# objects getting initiated with class (arguments should be the )
 object1 = Rectangle(160, 120, 2000)
 object2 = Rectangle(10, 120, 2000)
 
+
+method1 = object1.calculate_cost()
+
 print("Area of Rectangle object1 : %s sq units" % (object1.get_area()))
 print("Area of Rectangle object2 : %s sq units" % (object2.get_area()))
+
+print("Area Cost object1 : %s Euros" % (method1))
 
 
 ''' SAMPLE OUTPUT
 Area of Rectangle object1 : 19200 sq units
 Area of Rectangle object2 : 1200 sq units
+Area Cost Object1: 38400000 Euros
 '''
