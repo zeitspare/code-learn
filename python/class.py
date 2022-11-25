357634e2873e141aa4c8f36924154ad675b704de
zeitspare <96058214+zeitspare@users.noreply.github.com>
2022-11-25 13:37:26 +0100

    Basic class and object

--- python/class.py
+++ python/class.py
@@ -8,7 +8,16 @@ class Rectangle:
    def calculate_cost(self):
        area = self.get_area()
        return area * self.unit_cost
-      
+   
 # breadth = 120 units, length = 160 units, 1 sq unit cost = Rs 2000
-r = Rectangle(160, 120, 2000)
-print("Area of Rectangle: %s sq units" % (r.get_area()))
+object1 = Rectangle(160, 120, 2000)
+object2 = Rectangle(10, 120, 2000)
+
+print("Area of Rectangle object1 : %s sq units" % (object1.get_area()))
+print("Area of Rectangle object2 : %s sq units" % (object2.get_area()))
+
+
+''' SAMPLE OUTPUT
+Area of Rectangle object1 : 19200 sq units
+Area of Rectangle object2 : 1200 sq units
+'''
